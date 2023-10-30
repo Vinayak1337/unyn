067d773a9feec047c41f0651dfe05fc2bed912ac
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2023-10-31 04:09:03 +0530

    added two more sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 	return (
 		<html lang='en' className='scroll-smooth'>
 			<body
-				className={`relative flex flex-col w-screen min-h-screen overflow-x-hidden ${sourceSans.className}`}>
+				className={`relative scroll-smooth flex flex-col w-screen min-h-screen overflow-x-hidden ${sourceSans.className}`}>
 				<Navbar />
 				<JoinPopup />
 				<ToastContainer position='top-right' />
